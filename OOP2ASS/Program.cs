a8a48f534133bdf41fe7298f7208690e49acac37
Admin <Admin@SALMAAMRO>
2025-07-20 02:31:49 +0300

    Q1

--- OOP2ASS/Program.cs
+++ OOP2ASS/Program.cs
@@ -1,9 +1,64 @@
 ﻿namespace OOP2ASS
 {
+    #region Q1
+    //struct Rectangle
+    //{
+    //        #region Attributes 
+    //        private double widht;
+    //        private double height;
+    //        #endregion
+
+    //        #region Properties
+    //        public double Widht
+    //        {
+    //            get { return widht; }
+
+    //            set {
+    //            if (value >0)
+    //               widht = value;
+    //            else 
+    //                Console.WriteLine("Invalid width value");
+    //            }
+    //        }
+
+    //        public double Height
+    //        {
+    //            get { return height; }
+    //            set
+    //            {
+    //            if (value > 0)
+    //                height = value;
+    //            else
+    //                Console.WriteLine("Invalid height value");
+    //            }
+    //        }
+
+    //        public double Area
+    //        {
+    //           get { return widht * height; }
+    //        }
+    //    #endregion
+
+    //        #region Methods
+    //    public void DisplayInfo()
+    //    { 
+    //     Console.WriteLine ($"InfoOfRectangle \n Width:{widht},Height:{height}\n Area : {Area}");
+    //    }
+    //    #endregion
+    //}
+    #endregion
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            #region CallQ1
+            //Rectangle rectangle = new Rectangle();
+            //rectangle.Widht = 12;
+            //rectangle.Height = 6;
+            //rectangle.DisplayInfo();
+            #endregion
+
         }
     }
 }
