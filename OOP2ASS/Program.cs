61bc04b8658aa7f48df747e1be7c9b202dd618b6
Admin <Admin@SALMAAMRO>
2025-07-20 03:48:08 +0300

    Q2

--- OOP2ASS/Program.cs
+++ OOP2ASS/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace OOP2ASS
+﻿using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+
+namespace OOP2ASS
 {
     #region Q1
     //struct Rectangle
@@ -47,6 +50,79 @@
     //    #endregion
     //}
     #endregion
+
+    #region Q2
+    //#region Enums
+    //     enum GenderType
+    //     {
+    //        Male,Female
+    //     }
+
+    //     enum SecurityPrivileges 
+    //     {
+    //        guest, Developer, secretary , DBA
+    //     }
+    //    #endregion
+    //class Employee
+    //{
+    //    #region Attributes
+    //    private int id;
+    //    private string? name;
+    //    private double salary;
+    //    private DateTime hireDate;
+    //    private GenderType gender;
+    //    private SecurityPrivileges securityLevel;
+
+    //    #endregion
+
+    //    #region Properties
+    //    public int Id { set {id = value; }  get {return id; }}
+    //    public string? Name { set { name = value; } get { return name; } }
+    //    public double Salary
+    //    {
+    //        set
+    //        {
+    //            if (value > 0)
+    //                salary = value;
+    //            else
+    //                Console.WriteLine("Invalid salary value");
+                
+    //        }
+    //        get { return salary; }
+    //    }
+    //    public DateTime HireDate { set { hireDate = value; } get { return hireDate; } }
+
+    //    public GenderType Gender { 
+    //        set
+    //        {
+    //            gender = value; 
+    //        } 
+
+    //        get { return gender; }
+    //    }
+    //    public SecurityPrivileges SecurityLevel { set { securityLevel =value; } get {return securityLevel; } }
+
+
+
+
+    //    #endregion
+
+    //    #region Methods
+    //    override public string ToString()
+    //    {
+    //       return $"Employee Info:\n" +
+    //              $"ID: {id}\n" +
+    //              $"Name: {name}\n" +
+    //              $"Security Level: {securityLevel}\n" +
+    //              $"Salary: {string.Format("{0:C}", salary)}\n" +
+    //              $"Hire Date: {hireDate.ToShortDateString()}\n" +
+    //              $"Gender: {gender}";
+    //    }
+
+    //    #endregion
+
+    //}
+    #endregion
     internal class Program
     {
 
@@ -57,6 +133,19 @@
             //rectangle.Widht = 12;
             //rectangle.Height = 6;
             //rectangle.DisplayInfo();
+            #endregion
+
+            #region CallQ2
+            //Employee emp = new Employee();
+            //emp.Id = 1;
+            //emp.Name = "Karim Abdelghani";
+            //emp.Salary = 50.000;
+            //emp.HireDate = new DateTime(2004, 6, 23);
+            //emp.Gender = GenderType.Male;
+            //emp.SecurityLevel = SecurityPrivileges.Developer;
+
+            //Console.WriteLine(emp);
+
             #endregion
 
         }
