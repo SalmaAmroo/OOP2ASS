597df35dbbe29e5fd3cc74b7ed87082b2e26504b
Admin <Admin@SALMAAMRO>
2025-07-21 00:34:58 +0300

    Q4

--- OOP2ASS/Program.cs
+++ OOP2ASS/Program.cs
@@ -86,7 +86,7 @@ namespace OOP2ASS
     //                salary = value;
     //            else
     //                Console.WriteLine("Invalid salary value");
-                
+
     //        }
     //        get { return salary; }
     //    }
@@ -123,6 +123,219 @@ namespace OOP2ASS
 
     //}
     #endregion
+
+    #region Q3
+    //class HireDate
+    //{
+    //    //#region Attributes
+    //    //private int day;
+    //    //private int month;
+    //    //private int year;
+    //    //#endregion
+
+    //    //#region Properties
+    //    //public int Day
+    //    //{
+    //    //    get { return day; }
+    //    //    set
+    //    //    {
+    //    //        if (value >=1 && value <=31 )
+    //    //            day = value;
+    //    //        else
+    //    //            Console.WriteLine("Invalid day value");
+    //    //    }
+
+    //    //}
+    //    //public int Month
+    //    //{
+    //    //    get { return month; }
+    //    //    set
+    //    //    {
+    //    //        if (value >= 1 && value <= 12)
+    //    //            month = value;
+    //    //        else
+    //    //            Console.WriteLine("Invalid month value");
+    //    //    }
+
+    //    //}
+    //    //public int Year
+    //    //{
+    //    //    get { return year; }
+    //    //    set
+    //    //    {
+    //    //        if (value > 1900 && value <= DateTime.Now.Year)
+    //    //            year = value;
+    //    //        else
+    //    //            Console.WriteLine("Invalid year value");
+    //    //    }
+    //    //}
+    //    //#endregion
+
+    //    //#region Methods
+    //    //override public string ToString()
+    //    //{
+    //    //    return $"{day:D2}/{month:D2}/{year}";
+    //    //}
+    //    //#endregion
+
+
+    //}
+    #endregion
+
+    #region Q4
+    //    #region Enums
+    //[Flags]
+    //enum Permission
+    //{
+    //    Read = 1,
+    //    Write = 2,
+    //    Execute = 4,
+    //    Delete = 8,
+    //}
+    //enum SecurityLevel
+    //{
+    //    DBA, Guest, SecurityOfficer
+    //}
+    //#endregion
+    //class Employee
+    //{
+    //    #region Attributes
+    //    private int id;
+    //    private string? name;
+    //    private double salary;
+    //    private DateTime hireDate;
+    //    private SecurityLevel securityLevel;
+    //    private Permission permissions;
+    //    #endregion
+
+    //    #region Setters and Getters
+    //    public void SetId(int id)
+    //    {
+    //        this.id = id;
+    //    }
+    //    public int GetId()
+    //    {
+    //        return id;
+    //    }
+    //    public void SetName(string name)
+    //    {
+    //        this.name = name;
+    //    }
+    //    public string? GetName()
+    //    {
+    //        return name;
+    //    }
+    //    public void SetSalary(double salary)
+    //    {
+    //        this.salary = salary;
+    //    }
+    //    public double GetSalary()
+    //    {
+    //        return salary;
+    //    }
+    //    public void SetHireDate(DateTime hireDate)
+    //    {
+    //        this.hireDate = hireDate;
+    //    }
+    //    public DateTime GetHireDate()
+    //    {
+    //        return hireDate;
+    //    }
+    //    public void SetSecurityLevel(SecurityLevel securityLevel)
+    //    {
+    //        this.securityLevel = securityLevel;
+    //    }
+    //    public SecurityLevel GetSecurityLevel()
+    //    {
+    //        return securityLevel;
+    //    }
+    //    public void SetPermissions(Permission permissions)
+    //    {
+    //        this.permissions = permissions;
+    //    }
+    //    public Permission GetPermissions()
+    //    {
+    //        return permissions;
+    //    }
+
+    //    #endregion
+
+    //    #region Constructors
+
+    //    public Employee()
+    //    {
+    //        id = 0;
+    //        name = "";
+    //        salary = 0.0;
+    //        hireDate = DateTime.Now;
+    //        securityLevel = 0;
+    //        permissions = 0;
+    //    }
+    //    public Employee(int id, string name, double salary, DateTime hireDate, SecurityLevel securityLevel, Permission permissions)
+    //    {
+    //        this.id = id;
+    //        this.name = name;
+    //        this.salary = salary;
+    //        this.hireDate = hireDate;
+    //        this.securityLevel = securityLevel;
+    //        this.permissions = permissions;
+    //    }
+    //    #endregion
+
+    //    #region properties
+    //    public int Id
+    //    {
+    //        get { return id; }
+    //        set { id = value; }
+    //    }
+    //    public string? Name
+    //    {
+    //        get { return name; }
+    //        set { name = value; }
+    //    }
+    //    public double Salary
+    //    {
+    //        get { return salary; }
+    //        set
+    //        {
+    //            if (value > 0)
+    //                salary = value;
+    //            else
+    //                Console.WriteLine("Invalid salary value");
+    //        }
+    //    }
+    //    public DateTime HireDate
+    //    {
+    //        get { return hireDate; }
+    //        set { hireDate = value; }
+    //    }
+    //    public SecurityLevel SecurityLevel
+    //    {
+    //        get { return securityLevel; }
+    //        set { securityLevel = value; }
+    //    }
+    //    public Permission Permissions
+    //    {
+    //        get { return permissions; }
+    //        set { permissions = value; }
+    //    }
+    //    #endregion
+
+    //    #region Methods
+    //    public override string ToString()
+    //    {
+    //        return $"Employee Info:\n" +
+    //               $"ID: {id}\n" +
+    //               $"Name: {name}\n" +
+    //               $"Security Level: {securityLevel}\n" +
+    //               $"Salary: {string.Format("{0:C}", salary)}\n" +
+    //               $"Hire Date: {hireDate.ToShortDateString()}\n" +
+    //               $"Permissions: {permissions}";
+    //    }
+    //    #endregion
+
+    //}
+    #endregion
     internal class Program
     {
 
@@ -145,6 +358,28 @@ namespace OOP2ASS
             //emp.SecurityLevel = SecurityPrivileges.Developer;
 
             //Console.WriteLine(emp);
+
+            #endregion
+
+            #region CallQ3
+            //HireDate hireDate = new HireDate();
+            //hireDate.Day = 23;
+            //hireDate.Month = 6;
+            //hireDate.Year = 2004;
+            //Console.WriteLine($"Hire Date: {hireDate}");
+            #endregion
+
+            #region CallQ4
+            //Employee[] EmpArr = new Employee[3];
+            //EmpArr[0] = new Employee(1, "Karim", 50000, new DateTime(2020, 1, 1), SecurityLevel.SecurityOfficer, Permission.Read | Permission.Write | Permission.Execute | Permission.Delete);
+            //EmpArr[1] = new Employee(2, "Salma", 60000, new DateTime(2021, 2, 2), SecurityLevel.DBA, Permission.Read | Permission.Write);
+            //EmpArr[2] = new Employee(3, "Hoda", 70000, new DateTime(2022, 3, 3), SecurityLevel.Guest, Permission.Read);
+
+            //foreach (var emp in EmpArr)
+            //{
+            //    Console.WriteLine(emp);
+            //    Console.WriteLine();
+            //}
 
             #endregion
 
